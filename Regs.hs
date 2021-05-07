0e33a7a75dfc38f17fcee63d58ece6dd04926906
Jonas Jonas <jonson0180@yahoo.de>
2021-05-07 12:17:04 +0200

    Vereinfachung mit Print aufgesplittet in 2 getrennte Funktionen

--- Regs.hs
+++ Regs.hs
@@ -10,34 +10,85 @@
 --Ausdruck/Expression
 data Ausdruck = Epsilon | Phi | C String | Alternative Ausdruck Ausdruck | Konkatenation Ausdruck Ausdruck| Sternbildung Ausdruck deriving (Show)
 
-vereinfachung :: Ausdruck -> String
-vereinfachung (Phi) = "Phi"
-vereinfachung (Epsilon) = "Eps"
-vereinfachung (C a) = a
-vereinfachung (Konkatenation a Phi) = "Phi"
-vereinfachung (Konkatenation Phi a) = "Phi"
+
+
+
+vereinfachung :: Ausdruck -> Ausdruck
+vereinfachung (Phi) = Phi
+vereinfachung (Epsilon) = Epsilon
+vereinfachung (C a) = C a
+vereinfachung (Konkatenation a Phi) = Phi
+vereinfachung (Konkatenation Phi a) = Phi
 vereinfachung (Konkatenation a Epsilon) = vereinfachung a
 vereinfachung (Konkatenation Epsilon a) = vereinfachung a
-vereinfachung (Konkatenation a b) = (vereinfachung a) ++ " " ++ (vereinfachung b)
+vereinfachung (Konkatenation a b) = Konkatenation (vereinfachung a) (vereinfachung b)
 vereinfachung (Alternative a Phi) = vereinfachung a
 vereinfachung (Alternative Phi a) = vereinfachung a
-vereinfachung (Alternative a b) = if vereinfachung(a) == vereinfachung(b) then vereinfachung a else (vereinfachung a) ++ " + " ++ (vereinfachung b) 
-vereinfachung (Sternbildung Phi) = "Eps"
-vereinfachung (Sternbildung Epsilon) = "Eps"
+vereinfachung (Alternative a b) = if ((ausdruckPrinten (vereinfachung(a))) == (ausdruckPrinten (vereinfachung(a)))) then vereinfachung a else Alternative (vereinfachung a) (vereinfachung b) 
+vereinfachung (Sternbildung Phi) = Epsilon
+vereinfachung (Sternbildung Epsilon) = Epsilon
 vereinfachung (Sternbildung (Sternbildung a)) = vereinfachung (Sternbildung a)
-vereinfachung (Sternbildung a) = (vereinfachung a) ++ "*"
+vereinfachung (Sternbildung a) = Sternbildung (vereinfachung a)
+
+
+ausdruckPrinten :: Ausdruck -> String
+ausdruckPrinten (Phi) = "Phi"
+ausdruckPrinten (Epsilon) = "Eps"
+ausdruckPrinten (C a) = a
+ausdruckPrinten (Konkatenation a b) = (ausdruckPrinten a) ++ " " ++ (ausdruckPrinten b)
+ausdruckPrinten (Alternative a b) = (ausdruckPrinten a) ++ " + " ++ (ausdruckPrinten b) 
+ausdruckPrinten (Sternbildung a) = (ausdruckPrinten a) ++ "*"
+
+
+
+vereinfachungMitPrint :: Ausdruck -> String
+vereinfachungMitPrint (Phi) = "Phi"
+vereinfachungMitPrint (Epsilon) = "Eps"
+vereinfachungMitPrint (C a) = a
+vereinfachungMitPrint (Konkatenation a Phi) = "Phi"
+vereinfachungMitPrint (Konkatenation Phi a) = "Phi"
+vereinfachungMitPrint (Konkatenation a Epsilon) = vereinfachungMitPrint a
+vereinfachungMitPrint (Konkatenation Epsilon a) = vereinfachungMitPrint a
+vereinfachungMitPrint (Konkatenation a b) = (vereinfachungMitPrint a) ++ " " ++ (vereinfachungMitPrint b)
+vereinfachungMitPrint (Alternative a Phi) = vereinfachungMitPrint a
+vereinfachungMitPrint (Alternative Phi a) = vereinfachungMitPrint a
+vereinfachungMitPrint (Alternative a b) = if vereinfachungMitPrint(a) == vereinfachungMitPrint(b) then vereinfachungMitPrint a else (vereinfachungMitPrint a) ++ " + " ++ (vereinfachungMitPrint b) 
+vereinfachungMitPrint (Sternbildung Phi) = "Eps"
+vereinfachungMitPrint (Sternbildung Epsilon) = "Eps"
+vereinfachungMitPrint (Sternbildung (Sternbildung a)) = vereinfachungMitPrint (Sternbildung a)
+vereinfachungMitPrint (Sternbildung a) = (vereinfachungMitPrint a) ++ "*"
 
 --Beispiel aus Folie eps ((a*)* (phi + b))
 --let x = Konkatenation Epsilon (Konkatenation (Sternbildung (Sternbildung (C "a"))) (Alternative Phi (C "b")))
 
+--Beispiele aus folien 
+--let r3 = Alternative Phi (C "c")
+--let r4 = Alternative (C "c") Phi
+--let r5 = Sternbildung(Sternbildung(C "c"))
+--let r6 = Sternbildung Phi
+--let r7 = Alternative r3 r5
 
--- beispiel zum testen  Eps + "a" + (Phi | "b")
---main = do
-    --let beispielAusdruck = Konkatenation Epsilon Konkatenation C "a" Alternative Phi C "b"
-    --putStrLn vereinfachung beispielAusdruck
 
-  --  let beispielAusdruck = Konkatenation (C "abcd") (C "efgh")
-  --  vereinfachung beispielAusdruck
+--Teil Transitionen erstellen
+
+
+
+
+
+
+--Teil Ausführen
+
+
+--variante hülle aller spontanten übergänge bilden
+--Ausführen :: Automat String -> bool
+--ergebnis = kucken ob es zu zustandsnummer aus automat transition mit passendem zeichen gibt für jede die es gibt rekursiv Ausführen aufrufen 
+--paramter = zustandsnummer die nach der transition da ist + string verkürzt um das zeichen
+--wenn ein aufruf den ganzen string abarbeitet und in einem endzustand ankommt true zurückgeben - falls es ein dead end gibt false -> true wandert durch
+
+
+
+
+
 
 
     
---
+++ Überlegung.md
@@ -0,0 +1,144 @@
+
+
+annahme es werden nur bereits vereinfachte ausdrücke in automaten umgewandelt
+
+Regulärer Ausdruck wird als "objekt" erstellt mit Konstruktoren angegeben 
+aus Regulärem ausdruck wird automat mit transitionen erstellt
+programm muss mit Transitionen ermitteln können ob string teil des regulären ausdrucks ist
+
+
+
+## erstellen von transitionen
+es gibt eigentlich nur 3 pattern 
+
+# alternative
+a + b
+1 a 2 
+1 b 2
+start 1 ende 2
+
+# konkatenation
+a b
+1 a 2
+2 b 3
+start 1 ende 3
+
+# Sternbildung 
+1 a 2
+2 - 1
+1 - 2
+start 1 ende 2
+
+-> um loops/fehler von spontanen übergängen zu vermeiden 
+1 - 2
+2 a 3
+3 - 4
+1 - 4
+4 - 2
+
+
+
+# erstellen von großen ausdrücken durch verschachteln -> aus a/b werden andere Transitionen
+
+(a b)*
+
+Sternbildung(konkatenation((C "a") (C "b")))
+
+bsp abab
+
+start 1 ende 5
+
+1 - 2
+2 a 3
+3 b 4
+4 - 5
+5 - 2
+
+
+vorgehen rekursiv in ausdruck reingehen bis man auf ein C stößt unten angekommen
+je nach ausdruck 
+
+
+
+
+# datentyp transition step 1 funktion erstellt datenstruktur im nachfolgenden muster
+---->>> kann man sich warscheinlich schenken und stattdessen direkt die Regulärer Ausdruck datenstruktur verwenden
+
+Transition = Transition[Int,Transition,Int] | Transition[Int,Char,Int]
+
+Sternbildung(Konkatenation (C "a") (C "b"))
+
+
+Sternbildung 1
+    + transition 1 - 2
+    + transition 1 Konkatenation 2
+    + transition 2 - 1
+
+        Konkatenation
+        + transition 1 a 2
+        + transition 2 b 3
+
+bsp mit alternative
+
+a + b*
+
+Alternative
+    + transition 1 a 2
+    + transition 1 alt 2
+
+
+
+
+# umwandlung in step 2 -> lokale nach globale nummern umwandeln
+Transitionstep2 = Transition [int,char,int] ->>> evtl start und endzustand direkt in die struktur mit übernehmen
+
+funktion parameter Transitionsliste rückgabe Transitionsliste
+rückgabe ist immer transitionsliste die nichtmehr verschachtelt ist
+
+überspringe step 1,2,3 falls übergeben transitionsliste nicht verschachtelt ist
+
+    Step 1 führe funktion für erste untertransitionsliste aus (entfällt falls linker teil bereits nicht mehr verschachtelt ist)
+        rückgabe ist nicht verschachtelte liste
+        
+
+    Step 2 führe funktion für zweite untertransitionsliste aus (entfällt falls es sternbildung ist, oder falls rechter teil nicht mehr verschachtelt ist) 
+        rückgabe ist nicht verschachtelte liste
+
+    Step 3 
+        führe die zustandsnummern der 2 listen die aus step 1 und 2 zurückkamen zusammen mit denen aus dieser transition aka nummern anpassen
+            -> ist abhängig davon in was für einer eigenen transition man drin ist
+                -> evtl sinnvoll in erster struktur info mitzugeben in was für einem ausdrucksteil man grade ist zb.
+
+
+
+    Step 4 gebe nicht verschachtelte transitionsliste zurück
+
+
+# komplexeres beispiel
+
+(a b)* (c d)*
+
+Konkatenation((Sternbildung(Konkatenation (C "a") (C "b")))(Sternbildung(Konkatenation (C "c") (C "d"))) )
+
+
+Konkatenation 
++ transition 1 Sternbildung-1 2
++ transition 2 Sternbildung-2 3
+
+    Sternbildung 1
+    + transition 1 - 2
+    + transition 1 Konkatenation-1 2
+
+        Konkatenation 1
+        + transition 1 a 2
+        + transition 2 b 3
+
+    Sternbildung 2
+    + transition 1 - 2
+    + transition 1 Konkatenation-2 2
+
+        Konkatenation 2
+        + transition 1 c 2
+        + transition 2 d 3
+
+
